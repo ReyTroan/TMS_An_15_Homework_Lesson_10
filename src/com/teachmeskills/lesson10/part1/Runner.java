0ff1db227517f2bdd35343433c3010bb77282591
ReyTroan <reytroan@gmail.com>
2023-08-30 23:21:48 +0300

    adding method output to the Runner class

--- src/com/teachmeskills/lesson10/part1/Runner.java
+++ src/com/teachmeskills/lesson10/part1/Runner.java
@@ -3,6 +3,13 @@ package com.teachmeskills.lesson10.part1;
 public class Runner {
 
     public static void main(String[] args){
+        DocumentVerification.numericBlock("5551-AbC-5678-DeF-1a2B");
+        DocumentVerification.replacingLetters("5551-AbC-5678-DeF-1a2B");
         DocumentVerification.OnlyLettersToLower("5551-AbC-5678-DeF-1a2B");
+        DocumentVerification.OnlyLettersToUpper("5551-AbC-5678-DeF-1a2B");
+        DocumentVerification.numberSequenceLetter("5551-AbC-5678-DeF-1a2B");
+        DocumentVerification.startNumSequence("5551-AbC-5678-DeF-1a2B");
+        DocumentVerification.EndNumSequence("5551-AbC-5678-DeF-1a2B");
+
     }
 }
